99997e451fdd9d7a3c0b5bf50104c8407b645155
Asif Ahamad Shaik <asifahamadshaik@Asifs-MacBook-Air.local>
2024-12-02 00:04:24 +0530

    Kafka Listener

---
+++ cab-book-user/src/main/java/com/asiflearing/cabbookinguser/service/LocationService.java
@@ -0,0 +1,12 @@
+package com.asiflearing.cabbookinguser.service;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LocationService {
+    @KafkaListener(topics = "cab-location", groupId = "user-group")
+    public void locationListener(String location){
+        System.out.println(location);
+    }
+}
---
+++ cab-book-user/src/test/java/com/asiflearing/cabbookinguser/CabBookUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asiflearing.cabbookinguser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabBookUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
